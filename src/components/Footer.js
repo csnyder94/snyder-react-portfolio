5ff9003111dde66e5bede4c846c70f0c6ff5ac25
Christopher Snyder <snyder28139_04@hotmail.com>
2023-05-05 13:44:14 -0400

    Added new pages code, new image, and comments to code.  Also added package.json for github pages deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,30 @@
-//test
+import React from 'react'; // Importing React library
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importing FontAwesome Icons
+import { faGithub, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons'; // Importing specific icons used below
+
+const Footer = () => { 
+  return (
+    <footer className="bg-dark text-light py-3"> {/* Rendering the footer */}
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <p>&copy; Made by: Christopher Snyder</p>
+            <div className="social-icons">  {/* Rendering a FontAwesomeIcon with the GitHub, LinkedIn or Facebook icon */}
+              <a href="https://github.com/csnyder94"> 
+                <FontAwesomeIcon icon={faGithub} /> 
+              </a>
+              <a href="https://www.linkedin.com/in/christopher-snyder-a6000a246/"> 
+                <FontAwesomeIcon icon={faLinkedin} /> 
+              </a>
+              <a href="https://www.facebook.com/chris.snyder.792"> 
+                <FontAwesomeIcon icon={faFacebook} /> 
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer; // Exporting the 'Footer' component as the default export
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from "react"; // Importing React library and useEffect hook
 
-function Navigation(props) {
-  const { pages = [], setCurrentPage, currentPage } = props;
+function Nav(props) {
+  const { pages = [], setCurrentPage, currentPage } = props; // Destructuring props to get pages, setCurrentPage and currentPage
 
   useEffect(() => {
-    document.title = currentPage.name;
-  }, [currentPage]);
+    document.title = currentPage.name; // Updating the document title when the currentPage changes
+  }, [currentPage]); // Setting the dependency array to [currentPage] so that the effect only runs when the currentPage changes
 
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark"> {/* Rendering the navigation bar */}
       <div className="container-fluid">
-        <ul className="navbar-nav ms-auto mb-2 mb-md-0">
-          {pages.map((Page) => (
+        <ul className="navbar-nav ms-auto mb-2 mb-md-0"> {/* Rendering the unordered list for navigation links */}
+          {pages.map((Page) => ( // Mapping through the pages array to render navigation links
             <li
-              className={`nav-item ${
-                currentPage.name === Page.name && "active"
-              }`}
-              key={Page.name}
+              className={`nav-item ${currentPage.name === Page.name && "active"
+                }`} // Setting the active class on the navigation link if the currentPage matches the current page in the iteration
+              key={Page.name} // Setting the key for the navigation link
             >
               <span
                 className="nav-link"
-                onClick={() => setCurrentPage(Page)}
+                onClick={() => setCurrentPage(Page)} // Setting the current page when the navigation link is clicked
               >
                 {Page.name}
               </span>
@@ -32,4 +31,4 @@ function Navigation(props) {
   );
 }
 
-export default Navigation;
+export default Nav; // Exporting the 'Nav' component as the default export
