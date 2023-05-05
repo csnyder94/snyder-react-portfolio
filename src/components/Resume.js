2747bab607b9fc51d9b240776f956202a97d6b6f
Christopher Snyder <snyder28139_04@hotmail.com>
2023-05-05 17:42:34 -0400

    Added resume, picture, additional CSS, and about

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,1 +1,52 @@
-//Test
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <section id="resume">
+            <div className="container d-flex justify-content-center">
+                <div className="row justify-content-center">
+                    <p style={{ fontWeight: 'bold' }}>I have gained many new web development proficincies in this bootcamp; some of these include:</p>
+                    <div className="col-sm-6 py-4">
+                        <div className="resume-skills">
+                            <h4>Front-End Proficiencies:</h4>
+                            <ul>
+                                <li>HTML</li>
+                                <li>CSS/Bootstrap</li>
+                                <li>JavaScript/jQuery</li>
+                                <li>Web APIs</li>
+                                <li>Server-Side APIs</li>
+                                <li>Node.js</li>
+                                <li>React</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-6 py-4">
+                        <div className="resume-skills">
+                            <h4>Back-End Proficiencies:</h4>
+                            <ul>
+                                <li>MYSQL</li>
+                                <li>NoSQL</li>
+                                <li>MongoDB</li>
+                                <li>Model View Controller</li>
+                                <li>Progressive Web Applications</li>
+                                <li>Express.js</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-12 py-4 text-center">
+                <p style={{ fontWeight: 'bold' }}>To view my full resume, you may download a PDF version of it here:</p>
+                <div className="resume-download">
+                    <button className="btn btn-primary" onClick={() => window.open('https://docs.google.com/document/d/1Xhw2w1y21WcLseojaLKByZzMCpzYrocc/export?format=pdf', '_blank')}>
+                        Download Resume
+                    </button>
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default Resume;
